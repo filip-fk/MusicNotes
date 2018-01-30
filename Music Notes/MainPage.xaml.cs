e739916234d31ef4d9e7ffee737835b219ffe1be
Philip Kalapish <filipkalapish@outlook.com>
2018-01-30 22:30:05 +0100

    pi10

--- Music Notes/MainPage.xaml.cs
+++ Music Notes/MainPage.xaml.cs
@@ -35,6 +35,77 @@ namespace Music_Notes
             formattableTitleBar.BackgroundColor = Colors.Transparent;
             CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            pi_10.Text = CalculatePi(996);
+            transformNotes(pi_10.Text);
+        }
+
+        private void transformNotes(string notes)
+        {
+            char[] notesS = notes.ToCharArray();
+
+            foreach(var noteL in notesS)
+            {
+                if (noteL != '.')
+                {
+
+                }
+            }
+        }
+
+        public static string CalculatePi(int digits)
+        {
+            digits++;
+
+            uint[] x = new uint[digits * 10 / 3 + 2];
+            uint[] r = new uint[digits * 10 / 3 + 2];
+
+            uint[] pi = new uint[digits];
+
+            for (int j = 0; j < x.Length; j++)
+                x[j] = 20;
+
+            for (int i = 0; i < digits; i++)
+            {
+                uint carry = 0;
+                for (int j = 0; j < x.Length; j++)
+                {
+                    uint num = (uint)(x.Length - j - 1);
+                    uint dem = num * 2 + 1;
+
+                    x[j] += carry;
+
+                    uint q = x[j] / dem;
+                    r[j] = x[j] % dem;
+
+                    carry = q * num;
+                }
+
+
+                pi[i] = (x[x.Length - 1] / 10);
+
+
+                r[x.Length - 1] = x[x.Length - 1] % 10; ;
+
+                for (int j = 0; j < x.Length; j++)
+                    x[j] = r[j] * 10;
+            }
+
+            var result = "";
+
+            uint c = 0;
+
+            for (int i = pi.Length - 1; i >= 0; i--)
+            {
+                pi[i] += c;
+                c = pi[i] / 10;
+
+                result = (pi[i] % 10).ToString() + result;
+            }
+
+            string res10 = result.Remove(0, 1);
+            result = "3." + res10;
+            return result;
         }
     }
 }
